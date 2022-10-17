14a0e0c505022c5c302c8eac3d990e83ec588549
juecastroco <juecastroco11@hotmail.com>
2022-10-17 17:41:21 -0500

    Ejemplo APIREST

--- servidor.js
+++ servidor.js
@@ -1,1 +1,22 @@
-console.log("Hola Mundo desde Node.js")
+// console.log("Hola Mundo desde Node.js")
+require('dotenv').config()
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+const app = express();
+app.use(express.json());
+
+//Conexión a base de datos
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection
+db.on('error', (error) => console.log(error))
+db.once('open', () => console.log('Conectado a la base de datos'))
+
+
+const tareasRouter = require('./routes/tareas');
+app.use('/tareas', tareasRouter)
+app.listen(3000, () => {
+    console.log('Servidor corriendo en el puerto 3000')
+});
